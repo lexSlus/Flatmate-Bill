45e1c6b73b17caba7f4538bae0e8361968c2cb6b
LexSlus <sloboda276@gmail.com>
2022-07-15 15:58:20 +0300

    Train Project

---
+++ reports.py
@@ -0,0 +1,41 @@
+import webbrowser
+from fpdf import FPDF
+import os
+
+8175
+
+
+class PdfReport:
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def generate(self, flatmate1, flatmate2, bill):
+        flatmate1_pay = str(round(flatmate1.pays(bill, flatmate2), 2))
+        flatmate2_pay = str(round(flatmate2.pays(bill, flatmate1), 2))
+
+        pdf = FPDF(orientation='P', unit='pt', format='A4')
+        pdf.add_page()
+
+        # Add some text
+        pdf.image('files/house.jpg', w=50, h=50)
+
+        pdf.set_font(family='Times', size=24, style='B')
+        pdf.cell(w=0, h=80, txt="Flatmates Bill", border=0, align="C", ln=1)
+
+        # Insert Period label and value
+        pdf.set_font(family='Times', size=14, style='B')
+        pdf.cell(w=100, h=40, txt="Period:", border=0)
+        pdf.cell(w=150, h=40, txt=bill.period, border=0, ln=1)
+
+        # insert name and due amount of the first flatmate
+        pdf.set_font(family='Times', size=12)
+        pdf.cell(w=100, h=40, txt=flatmate1.name, border=0)
+        pdf.cell(w=150, h=40, txt=flatmate1_pay, border=0, ln=1)
+        # insert name and due amount of the first flatmate2
+        pdf.cell(w=100, h=40, txt=flatmate2.name, border=0)
+        pdf.cell(w=150, h=40, txt=flatmate2_pay, border=0, ln=1)
+        os.chdir('files')
+        pdf.output(self.filename)
+
+        webbrowser.open(self.filename)
